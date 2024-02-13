e816e620f961ce45f9ef57817f676b3be10e5cc5
Il tuo nome <svilupposoftware2023@a24pc04.studenti.engim.it>
2024-02-13 16:48:28 +0100

    Metodi statici

---
+++ Esercizi/src/Lezione4/metodi.java
@@ -0,0 +1,20 @@
+//Metodi statici
+
+package Lezione4;
+
+public class metodi {
+    public static int doppio(int n){
+        return n*2;
+    }
+    public static void main(String[] args)
+    /*
+    public
+    static -->Il metodo è statico e non serve il new
+    void -->ritorna niente
+    main --> nome del metodo
+    */
+
+    {
+
+    }
+}
